f9a37b94826b9abab0e85ab85a70478beccc72b6
Saar Sayfan <saarsayfan@gmail.com>
2024-12-02 00:16:49 -0800

    Initialize day1

---
+++ src/day1.py
@@ -0,0 +1,20 @@
+from utils import get_input
+
+input = get_input()
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
