9fa7827ae10472be58537d8ac57bfe00036e4e89
Saar Sayfan <saarsayfan@gmail.com>
2024-12-02 00:24:32 -0800

    Complete day1 part1

--- src/day1.py
+++ src/day1.py
@@ -1,10 +1,26 @@
+import bisect
+
 from utils import get_input
+
+
+SEPARATOR = "   "
 
 input = get_input()
 
 
 def part1():
-    pass
+    locations1 = []
+    locations2 = []
+    for line in input:
+        loc1, loc2 = line.split(SEPARATOR)
+        bisect.insort(locations1, int(loc1))
+        bisect.insort(locations2, int(loc2))
+
+    result = 0
+    for i in range(len(locations1)):
+        result += abs(locations1[i] - locations2[i])
+
+    print(result)
 
 
 def part2():
