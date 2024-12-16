9aec52752616ea9709d1ebc2d49912b57e8f367c
Saar Sayfan <saarsayfan@gmail.com>
2024-12-16 01:40:13 -0800

    Complete day11 part2

--- src/day11.py
+++ src/day11.py
@@ -30,8 +30,37 @@ def part1():
     print(len(stones))
 
 
+def cached_blink(stone, blinks, blink_cache):
+    if (stone, blinks) in blink_cache:
+        return blink_cache[(stone, blinks)]
+
+    stone_str = str(stone)
+    if blinks == 0:
+        result = 1
+    elif stone == 0:
+        result = cached_blink(1, blinks - 1, blink_cache)
+    elif len(stone_str) % 2 == 0:
+        midpoint = len(stone_str) // 2
+        stone1 = int(stone_str[:midpoint])
+        stone2 = int(stone_str[midpoint:])
+        result = cached_blink(stone1, blinks - 1, blink_cache) + cached_blink(
+            stone2, blinks - 1, blink_cache
+        )
+    else:
+        result = cached_blink(2024 * stone, blinks - 1, blink_cache)
+
+    blink_cache[(stone, blinks)] = result
+    return result
+
+
 def part2():
-    pass
+    stones = parse_input()
+    blink_cache = {}
+    result = 0
+    for stone in stones:
+        result += cached_blink(stone, 75, blink_cache)
+
+    print(result)
 
 
 def main():
